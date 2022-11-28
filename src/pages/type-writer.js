a66163e3a9c7019cbed3500e584696568859334b
Ryan <112320693+FalconChipp@users.noreply.github.com>
2022-11-28 15:42:26 +0000

    Gif test & animation tweaks

--- src/pages/type-writer.js
+++ src/pages/type-writer.js
@@ -1,9 +1,10 @@
 import React from "react";
 // import "./css/typewriter.css"
 import '../styles/type-new.css'
+import moneyEmoji from '../images/money-emoji-test-diffusion.gif'
 
 
-export default function Typewriter() {
+export default function Typewriter({ data }) {
     return (
         // <div className="container">
         //     <h3>Client</h3>
@@ -13,24 +14,29 @@ export default function Typewriter() {
         //     <p class="line-2 anim-typewriter-line2">Everyone's different... But we've made our</p> 
         //     <p class="line-3 anim-typewriter-line3">clients over £92 million so far...</p>
         // </div>
+        <div>
+            <section>
+                <div>
+                    <h3>Client:</h3>
+                    <p className="line-1">So how much money are you going to make me?</p>
+                </div>
 
-        <section>
-            <div>
-                <h3>Client:</h3>
-                <p className="line-1">So how much money are you going to make me?</p>
-            </div>
+                <div className="seek">
+                    <h3>SeekSocial:</h3>
+                    <p className="line-2">Everyone's different, But we've made our clients over £92 million so far...</p>
+                </div>
 
-            <div className="seek">
-                <h3>SeekSocial:</h3>
-                <p className="line-2">Everyone's different, But we've made our clients over £92 million so far...</p>
-            </div>
+                <div>
+                    <h3>Client:</h3>
+                    <p className="line-3">Holy S#*t...</p>
+                </div>
+            </section>
 
-            <div>
-                <h3>Client:</h3>
-                <p className="line-3">Holy S#*t...</p>
-            </div>
-            
-        </section>
+            <section className="emoji-cont">
+                <img src={moneyEmoji} alt="money emoji gif test" width={150}/>
+            </section>
+        </div>
 
     )
 }
+
